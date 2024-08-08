8a096c9f96e9dab39e5e0076b960a40d74ffd180
nate96 <natinael.96@gmail.com>
2024-08-08 14:13:16 +0300

    css fixes

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -79,7 +79,7 @@ return (
                     <h2>Submitted Data:</h2>
                     <p><strong>Name:</strong> {data.name}</p>
                     <p><strong>Email:</strong> {data.email}</p>
-                    <p><strong>Message:</strong> {data.message}</p>
+                    <p className="message"><strong>Message:</strong> {data.message}</p>
                 </div>
                 )}       
 
